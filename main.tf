16b88c8e0591dea96445f879973eb0a48cef0a7f
Andrew Reid <reid@sword>
2026-02-04 20:44:49 -0500

    Some skip entries for debugging.

--- main.tf
+++ main.tf
@@ -27,6 +27,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  skip_metadata_api_check = true
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
 }
 
 provider "cloudflare" {
